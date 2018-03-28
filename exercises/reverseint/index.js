23df1fb53ef9b7caf2f325c228c7f09cc2c84215
Andrii Savchuk <andriksr@gmail.com>
2018-03-28 17:19:19 +0300

    added reverseInt task

---
+++ exercises/reverseint/index.js
@@ -0,0 +1,13 @@
+// Given an integer, return an integer that is the reverse ordering of numbers.
+// Examples:
+//   reverseInt(12) === 21
+//   reverseInt(981) === 189
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+function reverseInt(number) {
+
+}
+
+let result = reverseInt(189);
+console.log(result);
