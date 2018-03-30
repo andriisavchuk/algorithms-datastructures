1c260f47aabcfccae1bf6d41920b8cddd36bbe04
Andrii Savchuk <andriksr@gmail.com>
2018-03-30 11:25:52 +0300

    maxchar

---
+++ exercises/maxchar/index.js
@@ -0,0 +1,11 @@
+// Given a string, return the character that is most commonly used in the string
+// Examples:
+//   maxChar("abccccccba") === "c"
+//   palindrome("abg 12311111111") === "1"
+
+function maxChar(str) {
+
+}
+
+let result = maxChar("abccccccba");
+console.log(result);
