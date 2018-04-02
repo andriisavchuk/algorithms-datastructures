f81cb24435ee834306a307fb8a32fcc4db86b011
Andrii Savchuk <andriksr@gmail.com>
2018-04-02 13:12:31 +0300

    max chars in string solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -4,8 +4,28 @@
 //   palindrome("abg 12311111111") === "1"
 
 function maxChar(str) {
+  let chars = {};
+  let maxValue = 0;
+  let maxChar = '';
+
+  for (const char of str) {
+    if (!chars[char]) {
+      chars[char] = 1;
+    } else {
+      chars[char]++;
+    }
+  }
+
+  for (const char in chars) {
+    if (chars[char] > maxValue) {
+      maxValue = chars[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
 
 }
 
 let result = maxChar("abccccccba");
-console.log(result);
+console.log(`Maximum chars in string have ${result}`);
