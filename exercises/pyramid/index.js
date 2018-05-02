f621da5c3e37b73ddbdeb534b740b7f9e83d5d56
Andrii Savchuk <andriksr@gmail.com>
2018-05-02 12:51:08 +0300

    little update

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,7 +14,7 @@ function pyramid(n) {
   const midpoint = Math.floor((2 * n - 1) / 2);
 
   for (let row = 0; row < n; row++) {
-    let level = '';
+    let level = "\'";
 
     for (let column = 0; column < 2 * n - 1 ; column++) {
        if (midpoint - row <= column && midpoint + row >= column) {
@@ -24,7 +24,7 @@ function pyramid(n) {
        }
      }
 
-     console.log(level);
+     console.log(level + "\'");
   }
 }
 
