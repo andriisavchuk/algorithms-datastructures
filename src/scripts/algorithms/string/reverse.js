377bed395453e5aa666cb8cfa1461b9c5a24d10f
Andrii Savchuk <andriksr@gmail.com>
2019-01-29 11:50:36 -0500

    reverse string with for loop

--- src/scripts/algorithms/string/reverse.js
+++ src/scripts/algorithms/string/reverse.js
@@ -1,1 +1,38 @@
-console.log('Hello String!');
+/*
+  Given a string, return a new string with the reversed order of characters
+  Example reverse('apple') === 'leppa'
+*/
+
+/*---- For loop ----*/
+
+/*
+  1.Create an empty string that will host the new created string
+
+  2.The starting point of the loop will be (str.length - 1) which
+    corresponds to the last character of the string, "o"
+    As long as i is greater than or equals 0, the loop will go on
+    We decrement i after each iteration
+
+    Here hello's length equals 5
+      For each iteration: i = str.length - 1 and newString = newString + str[i]
+      First iteration:    i = 5 - 1 = 4,         newString = "" + "o" = "o"
+      Second iteration:   i = 4 - 1 = 3,         newString = "o" + "l" = "ol"
+      Third iteration:    i = 3 - 1 = 2,         newString = "ol" + "l" = "oll"
+      Fourth iteration:   i = 2 - 1 = 1,         newString = "oll" + "e" = "olle"
+      Fifth iteration:    i = 1 - 1 = 0,         newString = "olle" + "h" = "olleh
+
+  3. Return the reversed string
+*/
+
+function reverseString(str) {
+  let reversedString = '';
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedString += str[i].toLowerCase();
+  }
+
+  return reversedString;
+}
+
+const result = reverseString('Apple');
+console.log(result);
