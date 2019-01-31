442a91404132bfd4615c958d4b1e63dd76a03914
Andrii Savchuk <andriksr@gmail.com>
2019-01-31 16:05:24 -0500

    reverse integer

---
+++ src/scripts/algorithms/math/reverseInt.js
@@ -0,0 +1,17 @@
+/*
+ Math.sign() - function returns the sign of a number,
+ indicating whether number is positive, negative or zero.
+*/
+
+function reverseInteger(n) {
+  const reversedNumber = n.toString().split('').reverse().join('');
+
+  // if (n < 0) {
+  //   return parseInt(reversedNumber) * -1;
+  // }
+
+  return parseInt(reversedNumber) * Math.sign(n);
+}
+
+let result = reverseInteger(-51);
+console.log(result);
