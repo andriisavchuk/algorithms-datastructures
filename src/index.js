24766cdd2ca7dd0c1accaf37ca7a493aa76f866f
Andrii Savchuk <andriksr@gmail.com>
2019-02-12 15:22:22 -0500

    calculate the sum of all elements in an array which has nested sub-arrays

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import './fonts/ArialRegular.ttf';
 import './index.html';
 import './index.sass';
 
-import './scripts/algorithms/math/chunkArrays';
+import './scripts/algorithms/math/sumArrayElements';
 
 // import './scripts/algorithms/string/maxChar';
