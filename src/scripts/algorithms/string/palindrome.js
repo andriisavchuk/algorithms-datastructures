c4c7f265a41435f0f4070bf88874735dbfbb58e1
Andrii Savchuk <andriksr@gmail.com>
2019-03-10 13:19:09 -0400

    refactor palindrome functions

--- src/scripts/algorithms/string/palindrome.js
+++ src/scripts/algorithms/string/palindrome.js
@@ -9,7 +9,6 @@
     palindrome("abcdefg") === false
 */
 
-
 function isPalindrome(str) {
   let reversedString = '';
 
@@ -27,9 +26,9 @@ function isPalindrome(str) {
 let result = isPalindrome('abba');
 console.log(result);
 
+/*------------------------------*/
 
-/*
-function isPalindrome(str) {
+function isPalindrome2(str) {
   let reversedString = str
     .split('')
     .reverse()
@@ -38,23 +37,23 @@ function isPalindrome(str) {
   return reversedString === str;
 }
 
-let result = isPalindrome('abba');
-console.log(result);
-*/
+let result2 = isPalindrome2('abba');
+console.log(result2);
 
-/*
-function isPalindrome(str) {
+/*------------------------------*/
+
+function isPalindrome3(str) {
   return str.split('').every((char, i) => {
     return char === str[str.length - i - 1];
   });
 }
 
-let result = isPalindrome('abbart');
-console.log(result);
-*/
+let result3 = isPalindrome3('abbart');
+console.log(result3);
 
-/*
-function isPalindrome(str) {
+/*------------------------------*/
+
+function isPalindrome4(str) {
   let charArray = [];
   let lettersArray = [];
   let validCharacters = 'abcdefghijklmnopqrstuvwxyz';
@@ -66,14 +65,8 @@ function isPalindrome(str) {
     if (validCharacters.indexOf(char) > -1) lettersArray.push(char);
   });
 
-  if (lettersArray.join('') === lettersArray.reverse().join('')) {
-    return true;
-  } else {
-    return false;
-  }
-
+  return lettersArray.join('') === lettersArray.reverse().join('');
 }
 
-let result = isPalindrome('Madam I`m Adam');
-console.log(result);
-*/
+let result4 = isPalindrome4('Madam I`m Adam');
+console.log(result4);
